51983e988be7c4e146df5decb5f56fab86fbc75b
CKKKKKKKKK <41709469+CKKKKKKKKK@users.noreply.github.com>
2020-07-06 16:49:32 +0800

    src
    frontend add src

---
+++ frontend/src/view/CustomerHome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {withRouter} from "react-router-dom";
+
+export default class CustomerHome extends React.Component{
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+// export default withRouter(CustomerHome);
+
