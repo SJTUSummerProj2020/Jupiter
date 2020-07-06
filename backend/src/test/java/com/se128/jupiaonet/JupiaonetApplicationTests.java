d8222a2d551c074fd7d7443dc07ef69e17754d88
HaoningLan <546004464@qq.com>
2020-07-06 16:05:08 +0800

    backend7.6

---
+++ backend/src/test/java/com/se128/jupiaonet/JupiaonetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se128.jupiaonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JupiaonetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
