2ef24b359210929e2c63113c5c57d9ee11ae86b4
shenhliu <55398344+shenhliu@users.noreply.github.com>
2020-07-07 15:01:42 +0800

    a

--- database/jupiter.sql
+++ database/jupiter.sql
@@ -17,44 +17,45 @@ drop table if exists user;
 /*==============================================================*/
 create table goods
 (
-      goods_id int not null
-      AUTO_INCREMENT,
-   surplus              int not null,
+   goods_id int not null
+   AUTO_INCREMENT,
+   
    name                 varchar
-      (32) not null,
+   (32) not null,
    place                varchar
-      (32) not null,
+   (32) not null,
    website              varchar
-      (1024) not null,
+   (1024) not null,
    goods_type           tinyint,
    primary key
-      (goods_id)
+   (goods_id)
 );
 
-      /*==============================================================*/
-      /* Table: goodsDetail                                           */
-      /*==============================================================*/
-      create table goodsDetail
-      (
-            detail_id int not null
-            AUTO_INCREMENT,
+   /*==============================================================*/
+   /* Table: goodsDetail                                           */
+   /*==============================================================*/
+   create table goodsDetail
+   (
+      detail_id int not null
+      AUTO_INCREMENT,
+            surplus              int not null,
    goods_id             int,
    price                float not null,
    time                 time not null,
    sessions             varchar
-            (32) not null,
+      (32) not null,
    ticket_type          tinyint not null,
    primary key
-            (detail_id)
+      (detail_id)
 );
 
-            /*==============================================================*/
-            /* Table: orderList                                             */
-            /*==============================================================*/
-            create table orderList
-            (
-                  order_id int NOT NULL
-                  AUTO_INCREMENT,
+      /*==============================================================*/
+      /* Table: orderList                                             */
+      /*==============================================================*/
+      create table orderList
+      (
+         order_id int NOT NULL
+         AUTO_INCREMENT,
    user_id              int not null,
    detail_id            int not null,
    source_id            int not null,
@@ -62,39 +63,39 @@ create table goods
    price                float not null,
    time                 time not null,
    primary key
-                  (order_id)
+         (order_id)
 );
 
-                  /*==============================================================*/
-                  /* Table: user                                                  */
-                  /*==============================================================*/
-                  create table user
-                  (
-                        user_id int not null
-                        AUTO_INCREMENT,
+         /*==============================================================*/
+         /* Table: user                                                  */
+         /*==============================================================*/
+         create table user
+         (
+            user_id int not null
+            AUTO_INCREMENT,
    user_name            varchar
-                        (32) not null,
+            (32) not null,
    password             varchar
-                        (32) not null,
+            (32) not null,
    phone                varchar
-                        (11) not null,
+            (11) not null,
    user_type            tinyint not null,
    primary key
-                        (user_id)
+            (user_id)
 );
 
-                        alter table goodsDetail add constraint FK_Reference_3 foreign key (goods_id)
+            alter table goodsDetail add constraint FK_Reference_3 foreign key (goods_id)
       references goods (goods_id)
-                        on delete restrict on
-                        update restrict;
+            on delete restrict on
+            update restrict;
 
-                        alter table orderList add constraint FK_Reference_2 foreign key (detail_id)
+            alter table orderList add constraint FK_Reference_2 foreign key (detail_id)
       references goodsDetail (detail_id)
-                        on delete restrict on
-                        update restrict;
+            on delete restrict on
+            update restrict;
 
-                        alter table orderList add constraint FK_Reference_4 foreign key (user_id)
+            alter table orderList add constraint FK_Reference_4 foreign key (user_id)
       references user (user_id)
-                        on delete restrict on
-                        update restrict;
+            on delete restrict on
+            update restrict;
 
