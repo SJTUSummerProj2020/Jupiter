8f088f62e3bef695866fad97ed789145ca83a533
CKKKKKKKKK <41709469+CKKKKKKKKK@users.noreply.github.com>
2020-07-08 10:59:54 +0800

    Add router

---
+++ frontend/src/Router.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Router, Route, Switch, Redirect} from 'react-router-dom';
+import {history} from "./utils/history";
+import {CustomerHome} from "./view/CustomerHome";
+import {DetailListView} from "./view/DetailListView";
+import {PersonalInfoView} from "./view/PersonalInfoView";
+import {OrderListView} from "./view/OrderListView";
+
+class BasicRoute extends React.Component{
+    constructor(props) {
+        super(props);
+        history.listen((location, action) => {
+            // clear alert on location change
+            console.log(location,action);
+        });
+    }
+
+    render() {
+        return(
+            <Router history={history}>
+                <Switch>
+                    <Route exact path="/" component={CustomerHome}/>
+                    <Route exact path="/goodsList" component={DetailListView}/>
+                    <Route exact path="/personalInfo" component={PersonalInfoView}/>
+                    <Route exact path="/orderList" component={OrderListView}/>
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+export default BasicRoute;
