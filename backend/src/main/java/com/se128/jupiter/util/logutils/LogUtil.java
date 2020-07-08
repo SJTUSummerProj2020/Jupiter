dabdf274ac4f7e2103dfa058092d9d048aea65ad
HaoningLan <546004464@qq.com>
2020-07-08 11:11:01 +0800

    feat:add log4j2 as log output, the logs will be stored in D:/logs

--- backend/src/main/java/com/se128/jupiter/util/logutils/LogUtil.java
+++ backend/src/main/java/com/se128/jupiter/util/logutils/LogUtil.java
@@ -4,15 +4,19 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class LogUtil {
-    private static final Logger logger = LogManager.getLogger(LogUtil.class.getName());
+    private static Logger logger= LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
     public void getHello() {
 
-        logger.trace("我是trace");
-        logger.info("我是info信息");
-        logger.error("我是error");
-        logger.fatal("我是fatal");
-
-        logger.trace("退出程序.");
+        for(int i=0;i<3;i++){
+            // 记录trace级别的信息
+            logger.trace("log4j2日志输出：This is trace message.");
+            // 记录debug级别的信息
+            logger.debug("log4j2日志输出：This is debug message.");
+            // 记录info级别的信息
+            logger.info("log4j2日志输出：This is info message.");
+            // 记录error级别的信息
+            logger.error("log4j2日志输出：This is error message.");
+        }
     }
 
     public static void main() {
