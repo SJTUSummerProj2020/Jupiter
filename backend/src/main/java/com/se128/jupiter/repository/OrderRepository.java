6b9b7792391a2eb97fa9b5e0dd2aaa0dfd20661b
HeyCat <monkey2000@gmail.com>
2020-07-09 16:40:20 +0800

    add test

--- backend/src/main/java/com/se128/jupiter/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.se128.jupiter.repository;
-
-import com.se128.jupiter.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-}
--- backend/src/main/java/com/se128/jupiter/service/OrderService.java
+++
@@ -1,7 +0,0 @@
-package com.se128.jupiter.service;
-
-import com.se128.jupiter.entity.Order;
-
-public interface OrderService {
-    Order addOrder(Order order);
-}
