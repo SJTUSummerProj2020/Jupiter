4c9d28ad9bbadd3c0cc26535cf70c67c95941516
CKKKKKKKKK <41709469+CKKKKKKKKK@users.noreply.github.com>
2020-07-10 08:45:09 +0800

    Fix
    Fix: Retain only one logo and fix header's style

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -98,7 +98,7 @@ export class Header extends React.Component{
                 <Link to={{pathname:'/'}}>
                     <div className="logo">
                         <img src={[require("../assets/planet.png")]} width={80} height={80} alt="Website logo"/>
-                        <img src={[require("../assets/logo2.png")]} width={200} height={80} alt="Website logo"/>
+                        {/*<img src={[require("../assets/logo2.png")]} width={200} height={80} alt="Website logo"/>*/}
                     </div>
                 </Link>
                 <div className="navigationList">
