fe67edd59fc8cc3e7d89c43451baf798d44f4eae
HaoningLan <546004464@qq.com>
2020-07-10 09:42:23 +0800

    docs
    add docs

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Switch, Redirect} from 'react-router-dom';
+import { Router, Route, Switch} from 'react-router-dom';
 import {history} from "./utils/history";
 import {CustomerHome} from "./view/CustomerHome";
 import {DetailListView} from "./view/DetailListView";
