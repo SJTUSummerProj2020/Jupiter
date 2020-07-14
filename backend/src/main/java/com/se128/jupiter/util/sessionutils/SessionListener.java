fdea7ec81eab5e1a6f6fa7599d2d62ca208b9c7c
HeyCat <monkey2000@gmail.com>
2020-07-14 15:20:25 +0800

    feat: add session listener to sessionutils

---
+++ backend/src/main/java/com/se128/jupiter/util/sessionutils/SessionListener.java
@@ -0,0 +1,29 @@
+package com.se128.jupiter.util.sessionutils;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import com.se128.jupiter.service.GoodsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SessionListener implements HttpSessionListener{
+    @Autowired
+    private GoodsService goodsService;
+
+    @Override
+    public void sessionCreated(HttpSessionEvent event){
+        HttpSession session = event.getSession();
+        // System.out.println("Session:" + session.hashCode());
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent event){
+        HttpSession session = event.getSession();
+        // String login_id = (String) session.getAttribute("userId");
+        goodsService.saveViewCounter();
+    }
+
+}
