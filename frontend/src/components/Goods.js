fbc17dcc3c2f6906b119a68ac43c29f6d76fa264
CKKKKKKKKK <41709469+CKKKKKKKKK@users.noreply.github.com>
2020-07-15 14:30:42 +0800

    Change data source of home to backend
    Feat: change data source of home page to backend

--- frontend/src/components/Goods.js
+++ frontend/src/components/Goods.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card } from 'antd';
 import "../css/goods.css"
+import {Link} from "react-router-dom";
 const { Meta } = Card;
 
 export class Goods extends React.Component{
@@ -10,13 +11,30 @@ export class Goods extends React.Component{
     render() {
         const {info} = this.props;
         return(
-            <Card
-                hoverable={true}
-                style={{width: 180, marginLeft: 30}}
-                cover={<img alt="image" src={info.image} className={"goodsImg"}/>}
+            <Link to={{
+                pathname: '/detail',
+                search: '?id=' + info.goodsId}}
+                  target="_blank"
             >
-                <Meta title={info.name} description={'¥' + info.price}/>
-            </Card>
+                <Card
+                    hoverable={true}
+                    style={{width: 180, marginLeft: 30}}
+                    cover={<img alt="image" src={info.image} className={"goodsImg"}/>}
+                >
+                    <Meta
+                        title={info.name}
+                        description={
+                            info.goodsDetails.length===0 ?
+                                (
+                                    <span className={"canceled"}>演出取消</span>
+                                ):
+                                (
+                                    <span>￥{info.goodsDetails[0].price}起</span>
+                                )
+                        }
+                    />
+                </Card>
+            </Link>
         );
     }
 }
