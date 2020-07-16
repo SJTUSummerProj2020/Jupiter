4bcdba856830859fa6f0a65fd7cf9573c6748908
CKKKKKKKKK <41709469+CKKKKKKKKK@users.noreply.github.com>
2020-07-16 15:58:48 +0800

    Change data source of RecommendationList to backend
    Feat: change data source of RecommendationList to backend

--- frontend/src/components/RecommendationList.js
+++ frontend/src/components/RecommendationList.js
@@ -1,33 +1,56 @@
 import React from "react";
 import {List} from 'antd';
 import {CalendarOutlined, HomeOutlined} from "@ant-design/icons";
+import {getRecommendGoods,checkSession} from "../services/userService";
 import '../css/recommendationlist.css';
-const listData = [
-    {image:require('../assets/goods/1.jpg'),name:"求婚女王",price:50,place:"上海大剧院",time:"2019.02.23-2021.02.22 "},
-    {image:require('../assets/goods/2.jpg'),name:"上海屋檐下",price:138,place:"上海大剧院",time:"2019.02.23-2021.02.22 "},
-    {image:require('../assets/goods/3.jpg'),name:"音乐剧《梵高》",price:100,place:"上海大剧院",time:"2019.02.23-2021.02.22 "},
-    {image:require('../assets/goods/4.png'),name:"夜猫俱乐部",price:100,place:"上海大剧院",time:"2019.02.23-2021.02.22 "},
-    {image:require('../assets/goods/5.png'),name:"千禧复古联盟 3.0 杭州站",price:80,place:"上海大剧院",time:"2019.02.23-2021.02.22 "},
-    {image:require('../assets/goods/6.png'),name:"[包邮包裹] 说唱拼盘",price:66,place:"上海大剧院",time:"2019.02.23-2021.02.22 "},
-    {image:require('../assets/goods/7.jpg'),name:"开心麻花爆笑舞台剧《乌龙山伯爵》",price:80,place:"上海大剧院",time:"2019.02.23-2021.02.22 "},
-    {image:require('../assets/goods/8.png'),name:"爆笑脱口秀演出-喜剧联盒国",price:150,place:"上海大剧院",time:"2019.02.23-2021.02.22 "},
-    {image:require('../assets/goods/9.jpg'),name:"太阳马戏《X绮幻之境》",price:360,place:"上海大剧院",time:"2019.02.23-2021.02.22 "},
-    {image:require('../assets/goods/10.png'),name:"魔女宅急便",price:100,place:"上海大剧院",time:"2019.02.23-2021.02.22 "}
-];
+
 export class RecommendationList extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={loggedIn:false,user:null,goodsList:[]}
+    }
+    componentDidMount() {
+        const checkSession_callback = (data) => {
+            if(data.status === 0){
+                this.setState(
+                    {
+                        loggedIn:true,
+                        user:data.data
+                    },
+                    ()=>{
+                        if(this.state.loggedIn === true){
+                            const data = {userId:this.state.user.userId,number:10};
+                            const callback = (data) => {
+                                console.log(data);
+                                this.setState(
+                                    {goodsList:data.data.goods}
+                                );
+                            }
+                            getRecommendGoods(data,callback);
+                        }
+                    }
+                );
+            }
+            else{
+                const data = {number:10};
+                const callback =(data) => {
+                    console.log(data);
+                    this.setState(
+                        {goodsList:data.data.goods}
+                    );
+                };
+                getRecommendGoods(data,callback);
+            }
+        };
+        checkSession(checkSession_callback);
+    }
 
     render() {
         return(
             <List
                 itemLayout="vertical"
                 size="small"
-                // pagination={{
-                //     onChange: page => {
-                //         console.log(page);
-                //     },
-                //     pageSize: 20,
-                // }}
-                dataSource={listData}
+                dataSource={this.state.goodsList}
                 renderItem={item => (
                     <List.Item>
                         <div className={"recommendation"}>
@@ -39,16 +62,27 @@ export class RecommendationList extends React.Component{
                             />
                             <div className={"recommendationDescription"}>
                                 <div className={"recommendationName"}>
-                                    <span>{item.name.length > 5 ? item.name.substr(0,5) + "..." : item.name}</span>
+                                    <span>{item.name.length > 12 ? item.name.substr(0,12) + "..." : item.name}</span>
                                 </div>
                                 <div className={"recommendationPlace"}>
-                                    <HomeOutlined/>{item.place}
+                                    <HomeOutlined/>{item.address.length > 18 ? item.address.substr(0,18) + "..." : item.address}
                                 </div>
                                 <div className={"recommendationTime"}>
-                                    <CalendarOutlined/>{item.time}
+                                    <CalendarOutlined/>
+                                    <span>
+                                        {item.startTime} - {item.endTime}
+                                    </span>
                                 </div>
                                 <div className={"recommendationPrice"}>
-                                    ￥{item.price}
+                                    {
+                                        item.goodsDetails.length === 0 ?
+                                            (
+                                                <span className={"canceled"}>演出取消</span>
+                                            ) :
+                                            (
+                                                <span>￥{item.goodsDetails[0].price}起</span>
+                                            )
+                                    }
                                 </div>
                             </div>
                         </div>
