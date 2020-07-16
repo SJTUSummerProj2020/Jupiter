32cec3827bf989198d7d0c4b74ab0c076a896b79
CKKKKKKKKK <41709469+CKKKKKKKKK@users.noreply.github.com>
2020-07-16 16:28:24 +0800

    Update RecommendationList.js
    Feat: add router in RecommendationList

--- frontend/src/components/RecommendationList.js
+++ frontend/src/components/RecommendationList.js
@@ -3,6 +3,7 @@ import {List} from 'antd';
 import {CalendarOutlined, HomeOutlined} from "@ant-design/icons";
 import {getRecommendGoods,checkSession} from "../services/userService";
 import '../css/recommendationlist.css';
+import {Link} from "react-router-dom";
 
 export class RecommendationList extends React.Component{
     constructor(props) {
@@ -53,39 +54,45 @@ export class RecommendationList extends React.Component{
                 dataSource={this.state.goodsList}
                 renderItem={item => (
                     <List.Item>
-                        <div className={"recommendation"}>
-                            <img
-                                width={100}
-                                className={"recommendationImg"}
-                                alt="cover"
-                                src={item.image}
-                            />
-                            <div className={"recommendationDescription"}>
-                                <div className={"recommendationName"}>
-                                    <span>{item.name.length > 12 ? item.name.substr(0,12) + "..." : item.name}</span>
-                                </div>
-                                <div className={"recommendationPlace"}>
-                                    <HomeOutlined/>{item.address.length > 18 ? item.address.substr(0,18) + "..." : item.address}
-                                </div>
-                                <div className={"recommendationTime"}>
-                                    <CalendarOutlined/>
-                                    <span>
+                        <Link to={{
+                            pathname: '/detail',
+                            search: '?id=' + item.goodsId}}
+                              target="_blank"
+                        >
+                            <div className={"recommendation"}>
+                                <img
+                                    width={100}
+                                    className={"recommendationImg"}
+                                    alt="cover"
+                                    src={item.image}
+                                />
+                                <div className={"recommendationDescription"}>
+                                    <div className={"recommendationName"}>
+                                        <span>{item.name.length > 12 ? item.name.substr(0,12) + "..." : item.name}</span>
+                                    </div>
+                                    <div className={"recommendationPlace"}>
+                                        <HomeOutlined/>{item.address.length > 18 ? item.address.substr(0,18) + "..." : item.address}
+                                    </div>
+                                    <div className={"recommendationTime"}>
+                                        <CalendarOutlined/>
+                                        <span>
                                         {item.startTime} - {item.endTime}
                                     </span>
-                                </div>
-                                <div className={"recommendationPrice"}>
-                                    {
-                                        item.goodsDetails.length === 0 ?
-                                            (
-                                                <span className={"canceled"}>演出取消</span>
-                                            ) :
-                                            (
-                                                <span>￥{item.goodsDetails[0].price}起</span>
-                                            )
-                                    }
+                                    </div>
+                                    <div className={"recommendationPrice"}>
+                                        {
+                                            item.goodsDetails.length === 0 ?
+                                                (
+                                                    <span className={"canceled"}>演出取消</span>
+                                                ) :
+                                                (
+                                                    <span>￥{item.goodsDetails[0].price}起</span>
+                                                )
+                                        }
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </Link>
                     </List.Item>
                 )}
             />
