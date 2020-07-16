03ce2db82c8e9fd674ceb88c773ff4ba93337b52
CKKKKKKKKK <41709469+CKKKKKKKKK@users.noreply.github.com>
2020-07-16 16:59:02 +0800

    Use status of session from props in Recommendation
    Fix: use status of session from props in Recommendation

--- frontend/src/components/RecommendationList.js
+++ frontend/src/components/RecommendationList.js
@@ -1,49 +1,31 @@
 import React from "react";
 import {List} from 'antd';
 import {CalendarOutlined, HomeOutlined} from "@ant-design/icons";
-import {getRecommendGoods,checkSession} from "../services/userService";
+import {getRecommendGoods} from "../services/userService";
 import '../css/recommendationlist.css';
 import {Link} from "react-router-dom";
 
 export class RecommendationList extends React.Component{
     constructor(props) {
         super(props);
-        this.state={loggedIn:false,user:null,goodsList:[]}
+        this.state={goodsList:[]}
     }
     componentDidMount() {
-        const checkSession_callback = (data) => {
-            if(data.status === 0){
-                this.setState(
-                    {
-                        loggedIn:true,
-                        user:data.data
-                    },
-                    ()=>{
-                        if(this.state.loggedIn === true){
-                            const data = {userId:this.state.user.userId,number:10};
-                            const callback = (data) => {
-                                console.log(data);
-                                this.setState(
-                                    {goodsList:data.data.goods}
-                                );
-                            }
-                            getRecommendGoods(data,callback);
-                        }
-                    }
-                );
-            }
-            else{
-                const data = {number:10};
-                const callback =(data) => {
-                    console.log(data);
-                    this.setState(
-                        {goodsList:data.data.goods}
-                    );
-                };
-                getRecommendGoods(data,callback);
-            }
+        const callback = (data) => {
+            this.setState(
+                {
+                    goodsList:data.data.goods
+                }
+            );
         };
-        checkSession(checkSession_callback);
+        if(this.props.loggedIn){
+            const data = {number:10,userId:this.props.user.userId};
+            getRecommendGoods(data,callback);
+        }
+        else{
+            const data = {number:10};
+            getRecommendGoods(data,callback);
+        }
     }
 
     render() {
